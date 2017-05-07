174f8bb4ca6a75905360d76a855dd7491155f4bc
Alan Munguía <alan.munguia@kimanta.com>
2017-05-06 20:04:49 -0500

    Added -D flag to debug messages

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+import sys
+
 from lexAnalyzer import Lex, writeSuccessFile
 from errorManager import LexError, writeErrorFile
 
 lex = Lex(file_path='entrada.txt')
-
 try:
     lex.tokenize()
-    for tok in lex.tokens:
+    if '-D' in sys.argv:
         print(tok)
 except LexError as e:
+    print(e)
     writeErrorFile()
 else:
     writeSuccessFile()
